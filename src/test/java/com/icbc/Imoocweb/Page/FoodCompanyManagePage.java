f8f0fe4b7a4e028b44229697b34612d7bbc9ff86
huahua_git <946308446@qq.com>
2021-12-14 10:52:05 +0800

    修改配餐公司page.class文件的sendkey方法

--- src/test/java/com/icbc/Imoocweb/Page/FoodCompanyManagePage.java
+++ src/test/java/com/icbc/Imoocweb/Page/FoodCompanyManagePage.java
@@ -31,31 +31,60 @@ public class FoodCompanyManagePage extends BasePage{
 		logger.debug("获取配餐公司名称元素");
 		return GetElement("foodcompany_name_ele");
 	}	
+	//配餐公司名称sendkey
+	public String SendkeyFoodCompName() throws Exception {
+		logger.debug("配餐公司名称sendkey");
+		return GetEleSendKey("foodcompany_name_ele");
+	}
 	//获取配餐公司子商户ID元素
 	public WebElement GetFoodCompNnmberEle() throws Exception {
 		logger.debug("获取配餐公司子商户ID元素");
 		return GetElement("merchantID_ele");
 	}	
+	//配餐公司子商户ID
+	public void SendkeyFoodCompID() throws Exception {
+		logger.debug("配餐公司子商户ID sendkey");
+	}	
 	//获取配餐公司联系人元素
 	public WebElement GetFoodCompContactEle() throws Exception {
 		logger.debug("获取配餐公司联系人元素");
 		return GetElement("contact_person_ele");
-	}		
+	}
+	//配餐公司联系人sendkey
+	public String SendkeyFoodCompContact() throws Exception {
+		logger.debug("配餐公司联系人sendkey");
+		return GetEleSendKey("contact_person_ele");
+	}
 	//获取配餐公司手机号元素
 	public WebElement GetFoodCompMobileEle() throws Exception {
 		logger.debug("获取配餐公司手机号元素");
 		return GetElement("mobile_ele");
-	}		
+	}	
+	//配餐公司手机号sendkey
+	public void SendkeyMobile() {
+		logger.debug("配餐公司手机号sendkey");
+	}
+	
 	//获取配餐公司邮箱元素
 	public WebElement GetFoodCompEmailEle() throws Exception {
 		logger.debug("获取配餐公司邮箱元素");
 		return GetElement("email_ele");
-	}		
+	}	
+	//配餐公司邮箱sendkey
+	public String SendkeyFoodCompMail() throws Exception {
+		logger.debug("配餐公司邮箱sendkey");
+		return GetEleSendKey("email_ele");
+	}
 	//获取配餐公司地址元素
 	public WebElement GetFoodCompAddressEle() throws Exception {
 		logger.debug("获取配餐公司地址元素");
 		return GetElement("address_ele");
 	}	
+	//配餐公司地址sendkey
+	public String SendkeyFoodCompAdress() throws Exception {
+		logger.debug("配餐公司地址sendkey");
+		return GetEleSendKey("address_ele");
+	}
 	//获取新增-保存元素
 	public WebElement GetFoodCompSaveEle() throws Exception {
 		logger.debug("获取新增-保存元素");
@@ -85,12 +114,22 @@ public class FoodCompanyManagePage extends BasePage{
 	public WebElement GetEditFoodCompNameEle() throws Exception {
 		logger.debug("获取修改配餐公司名称元素");
 		return GetElement("edit_companyname_ele");
-	}		
+	}
+	//修改配餐公司名称sendkey
+	public String SendkeyEditFoodCompName() throws Exception {
+		logger.debug("修改配餐公司名称sendkey");
+		return GetEleSendKey("edit_companyname_ele");
+	}
 	//获取修改配餐公司联系人元素
 	public WebElement GetEditFoodCompContactEle() throws Exception {
 		logger.debug("获取修改配餐公司联系人元素");
 		return GetElement("edit_contact_ele");
-	}	
+	}
+	//修改配餐公司联系人sendkey
+	public String SendkeyEditFoodCompContact() throws Exception {
+		logger.debug("修改配餐公司联系人sendkey");
+		return GetEleSendKey("edit_contact_ele");
+	}
 	//获取修改配餐公司手机号元素
 	public WebElement GetEditFoodCompMobileEle() throws Exception {
 		logger.debug("获取修改配餐公司手机号元素");
@@ -101,12 +140,22 @@ public class FoodCompanyManagePage extends BasePage{
 		logger.debug("获取修改配餐公司邮箱元素");
 		return GetElement("edit_email_ele");
 	}	
-	
+	//修改配餐公司邮箱sendkey
+	public String SendkeyEditFoodCompMail() throws Exception {
+		logger.debug("修改配餐公司邮箱sendkey");
+		return GetEleSendKey("edit_email_ele");
+	}
 	//获取修改配餐公司地址元素
 	public WebElement GetEditFoodCompAddressEle() throws Exception {
 		logger.debug("获取修改配餐公司地址元素");
 		return GetElement("edit_address_ele");
-	}		
+	}	
+	
+	//修改配餐公司地址sendkey
+	public String SendkeyEditFoodCompAdress() throws Exception {
+		logger.debug("修改配餐公司地址sendkey");
+		return GetEleSendKey("edit_address_ele");
+	}	
 	//获取修改保存元素
 	public WebElement GetEditFoodCompSaveEle() throws Exception {
 		logger.debug("获取修改保存元素");
