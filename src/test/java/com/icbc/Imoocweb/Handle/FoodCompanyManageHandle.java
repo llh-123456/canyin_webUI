9fa9e71e9f07f39e11927223931b95b980c8cbaf
huahua_git <946308446@qq.com>
2021-12-14 15:37:01 +0800

    新增配餐公司handle.class

--- src/test/java/com/icbc/Imoocweb/Handle/FoodCompanyManageHandle.java
+++ src/test/java/com/icbc/Imoocweb/Handle/FoodCompanyManageHandle.java
@@ -2,12 +2,54 @@ package com.icbc.Imoocweb.Handle;
 
 import org.openqa.selenium.WebDriver;
 
-public class FoodCompanyManageHandle extends BaseHandle{
+import com.icbc.Imoocweb.Page.FoodCompanyManagePage;
 
+public class FoodCompanyManageHandle extends BaseHandle{
+	FoodCompanyManagePage foodcompanypage;
+	//构造器；初始化page页面
 	public FoodCompanyManageHandle(WebDriver driver) {
 		super(driver);
-		
-		
+		foodcompanypage = new FoodCompanyManagePage(driver);
 	}
+	//点击市场区域菜单
+	
+	//点击配餐公司菜单
+	
+	//点击新增配餐公司按钮
+	
+	//点击输入配餐公司名称
+	
+	//点击输入配餐公司子商户id
+	
+	//点击输入配餐公司联系人
+	
+	//点击输入配餐公司手机号
+	
+	//点击输入配餐公司邮箱
+	
+	//点击输入配餐公司地址
+	
+	//点击新增-保存
+	
+	//点击输入框
+	
+	//点击搜索
+	
+	
+	//点击选中
+	
+	//点击修改
+	
+	//点击修改配餐公司名称
+	
+	//点击修改配餐公司联系人
+	
+	
+	//点击修改配餐公司手机号
+	
+	//点击修改配餐公司邮箱
+	
+	//点击修改配餐公司地址
+	
 
 }
